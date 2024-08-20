a09350b84b214559578c00095b3f56045420fdb3
zasweted <zaswet@gmail.com>
2024-08-20 22:28:10 +0300

    footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import './footer.css';
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">ALEXTHEDEV</a>
+      <ul className="footer__permalinks">
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#services">Services</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#testimonials">Testimonials</a>
+        <a href="#contacts">Contacts</a>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://linkedin.com" target="_blank">
+          <FaLinkedin />
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <FaFacebookSquare />
+        </a>
+        <a href="https://github.com" target="_blank">
+          <FaGithubSquare />
+        </a>
+      </div>
+      <div className="footer__copyrights">
+        <small>
+          &copy; 2024 ALEXTHEDEV | All rights reserved.
+        </small>
+      </div>
+    </footer>
   )
 }
 
